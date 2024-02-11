fe8920846972e2baafbda69f1395497ffb134cc5
João Eudes Lima <joaoeudes7@gmail.com>
2024-02-11 15:56:10 -0300

    added others properties

---
+++ lib/src/main/java/com/jedev/countries/api/models/EnumContinentModel.kt
@@ -0,0 +1,11 @@
+package com.jedev.countries.api.models
+
+enum class EnumContinentModel(val value: String) {
+    Africa("Africa"),
+    Antarctica("Antarctica"),
+    Asia("Asia"),
+    Europe("Europe"),
+    NorthAmerica("North America"),
+    Oceania("Oceania"),
+    SouthAmerica("South America");
+}
---
+++ lib/src/main/java/com/jedev/countries/api/models/TranslationModel.kt
@@ -0,0 +1,6 @@
+package com.jedev.countries.api.models
+
+data class TranslationModel (
+    val official: String,
+    val common: String
+)
